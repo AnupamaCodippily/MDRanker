3c9350a1931f31b64e56b09cce5eedce65660b9b
Anupama Codippily <Anupama Codippily@DESKTOP-HS2K404>
2020-04-08 14:54:59 +0530

    minor bug fix

--- MDRanker/src/server/ToolsetHandler.java
+++ MDRanker/src/server/ToolsetHandler.java
@@ -1,12 +1,13 @@
 package server;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.HashMap;
 import java.util.Vector;
 
 public interface ToolsetHandler extends Remote {
 
-	public Vector<HashMap<String, String>> ExecuteRawFetchQuery(String rawQuery) ;
-	public Vector<HashMap<String, String>> ExecuteRawUpdateQuery(String rawQuery) ;
+	public Vector<HashMap<String, String>> ExecuteRawFetchQuery(String rawQuery) throws RemoteException;
+	public Vector<HashMap<String, String>> ExecuteRawUpdateQuery(String rawQuery) throws RemoteException;
 	
 }
